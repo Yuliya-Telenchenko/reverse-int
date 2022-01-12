23f6cc87de069cf59f1a56d9332f6dce9c82059a
Yulia <yuliya.bytsanets@gmail.com>
2022-01-13 00:33:41 +0300

    feat: index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (number) {
+    let stringNumber = String(Math.abs(number));
+    let newNumber = '';
+    for (let i = stringNumber.length - 1; i >= 0; i--){
+        newNumber += stringNumber[i];
+    }
+    return +newNumber;
 }
